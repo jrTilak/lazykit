ad266c50a7db723754cf12e3540e7f9d32f9f554
Tilak Thapa <iamjrtilak@gmail.com>
2024-04-05 07:58:47 +0545

    feat: add async function generateStaticParams to page.tsx

--- src/www/src/app/(pages)/docs/[...slug]/page.tsx
+++ src/www/src/app/(pages)/docs/[...slug]/page.tsx
@@ -1,7 +1,7 @@
 import NotFound from "@/components/pages/not-found";
 import PAGES_CONFIG from "./_configs/_pages.config";
 import MethodPage from "./_pages/method-page";
-
+import registry from "@/configs/prev-next-button-links.json";
 const DocsPage = ({ params: { slug } }: { params: { slug: string[] } }) => {
   const predefinedPage = PAGES_CONFIG.find(
     (page) => page.path === `/docs/${slug.join("/")}`
@@ -15,3 +15,9 @@ const DocsPage = ({ params: { slug } }: { params: { slug: string[] } }) => {
   return <NotFound />;
 };
 export default DocsPage;
+
+export async function generateStaticParams() {
+  const urls = registry.map((item) => item.url.replace("/docs", "").split("/"));
+  const params = urls.map((url) => ({ params: { slug: url } }));
+  return params;
+}
