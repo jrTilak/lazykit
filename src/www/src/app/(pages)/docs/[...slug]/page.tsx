122ae171e825b71228124b25626bbbf9d3fb5fb9
Tilak Thapa <iamjrtilak@gmail.com>
2024-04-09 10:59:24 +0545

    fix: add build:www script to package.json and fix URL replacement in generateStaticParams

--- src/www/src/app/(pages)/docs/[...slug]/page.tsx
+++ src/www/src/app/(pages)/docs/[...slug]/page.tsx
@@ -17,6 +17,6 @@ const DocsPage = ({ params: { slug } }: { params: { slug: string[] } }) => {
 export default DocsPage;
 
 export async function generateStaticParams() {
-  const urls = registry.map((item) => item.url.replace("/docs", "").split("/"));
+  const urls = registry.map((item) => item.url.replace("/docs/", "").split("/"));
   return urls.map((url) => ({ slug: url }));
 }
