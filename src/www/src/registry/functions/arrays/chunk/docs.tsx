e293d1bf8b67381f4c19362c295cb65275d2232f
Tilak Thapa <iamjrtilak@gmail.com>
2024-04-10 23:44:22 +0545

    fix: &apos;

--- src/www/src/registry/functions/arrays/chunk/docs.tsx
+++ src/www/src/registry/functions/arrays/chunk/docs.tsx
@@ -5,7 +5,7 @@ const Docs = () => {
     <div className="flex flex-col gap-2">
       <p>
         The chunk function is a utility function in JavaScript that takes an
-        array and divides it into smaller sub-arrays, or "chunks", each with a
+        array and divides it into smaller sub-arrays, or &quot;chunks&quot;, each with a
         maximum length equal to a specified size. This function is particularly
         useful when you need to process a large array in smaller, more
         manageable pieces.
@@ -13,7 +13,7 @@ const Docs = () => {
       <p>
         The function accepts three parameters: the original array to be chunked,
         the size of each chunk, and a boolean value indicating whether the
-        function should strictly adhere to the chunk size. If the 'strict'
+        function should strictly adhere to the chunk size. If the &quot;strict&quot;
         parameter is set to true, and the last chunk does not meet the specified
         size, it will be removed from the final result.
       </p>
