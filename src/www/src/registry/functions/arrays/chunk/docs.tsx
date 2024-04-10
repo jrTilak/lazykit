cfc05452d1ffe61cc5b92da982b17fcf138d509c
Tilak Thapa <iamjrtilak@gmail.com>
2024-04-10 22:23:23 +0545

    fix: remove external link

--- src/www/src/registry/functions/arrays/chunk/docs.tsx
+++ src/www/src/registry/functions/arrays/chunk/docs.tsx
@@ -24,20 +24,6 @@ export default Docs;
 
 export const Info: IDoc = {
   description: "Chunks an array into smaller arrays of a specified size.",
-  externalLinks: [
-    {
-      label: "lodash",
-      url: "https://lodash.com/docs/4.17.15#chunk",
-    },
-    {
-      label: "lodash",
-      url: "https://lodash.com/docs/4.17.15#chunk",
-    },
-    {
-      label: "lodash",
-      url: "https://lodash.com/docs/4.17.15#chunk",
-    },
-  ],
 };
 
 export const Props: IRegistryFunctionPropTable[] = [
