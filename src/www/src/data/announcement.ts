cdc9dad7de501beafad2ad7a474a80962d877377
Tilak Thapa <iamjrtilak@gmail.com>
2024-04-28 07:48:39 +0545

    chore: update version number

--- src/www/src/data/announcement.ts
+++ src/www/src/data/announcement.ts
@@ -2,5 +2,5 @@ import { PartyPopper } from "lucide-react";
 export const ANNOUNCEMENT_DATA = {
   url: "/docs/introduction",
   icon: PartyPopper,
-  title: "Initial Release! @v1.0.0",
+  title: "New Release! @v1.0.2",
 };
