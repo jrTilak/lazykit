d2591c3603772d2821dfe69da8ca78d6685ada39
Tilak Thapa <tilakthapa9898@gmail.com>
2024-05-14 17:55:06 +0545

    fix: remove flag form installation code block

--- src/www/src/app/(pages)/docs/_pages/change-log.tsx
+++ src/www/src/app/(pages)/docs/_pages/change-log.tsx
@@ -41,7 +41,7 @@ const ChangeLog = async () => {
             <div className="flex flex-col gap-3">
               <h3
                 className="text-lg sm:text-xl lg:text-2xl font-semibold flex gap-2"
-                id="introduction"
+                id={pr.title.replace(/ /g, "-")}
               >
                 <Link target="_blank" href={pr.html_url} className="min-w-max">
                   <span className="hover:underline">
