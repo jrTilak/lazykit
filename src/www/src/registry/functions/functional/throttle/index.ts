5a214e85ad9f68581e2b0b07f396a2e276498b47
jrtilak <iamjrtilak@gmail.com>
2024-07-23 08:56:37 +0545

    feat: add throttle method

---
+++ src/www/src/registry/functions/functional/throttle/index.ts
@@ -0,0 +1,15 @@
+function throttle<A extends any[]>(
+  fn: (...args: A) => void,
+  limit: number
+): (...args: A) => void {
+  let lastCall = 0;
+  return (...args: A) => {
+    const now = Date.now();
+    if (now - lastCall >= limit) {
+      lastCall = now;
+      fn(...args);
+    }
+  };
+}
+
+export default throttle;
