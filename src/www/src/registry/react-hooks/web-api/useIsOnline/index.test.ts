296d9d46af40075289ab42a03453a687e9b06605
jrTilak <tilakthapa9898@gmail.com>
2024-11-10 20:03:14 +0545

    feat: add useIsOnline hook

---
+++ src/www/src/registry/react-hooks/web-api/useIsOnline/index.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from "vitest";
+
+describe("no test", () => {
+  it("should be true", () => {
+    expect(true);
+  });
+});
---
+++ src/www/src/registry/react-hooks/web-api/useIsOnline/index.ts
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+
+/**
+ * A custom hook to track the online/offline status of the user.
+ */
+const useIsOnline = () => {
+  const [isOnline, setIsOnline] = useState<boolean>(navigator.onLine);
+
+  const updateOnlineStatus = () => {
+    setIsOnline(true);
+  };
+
+  const updateOfflineStatus = () => {
+    setIsOnline(false);
+  };
+
+  useEffect(() => {
+    // Listen to the online and offline events
+    window.addEventListener("online", updateOnlineStatus);
+    window.addEventListener("offline", updateOfflineStatus);
+
+    // Cleanup the event listeners when the component unmounts
+    return () => {
+      window.removeEventListener("online", updateOnlineStatus);
+      window.removeEventListener("offline", updateOfflineStatus);
+    };
+  }, []);
+
+  return isOnline;
+};
+
+export default useIsOnline;
