badfc7ecf4880f47c15844e53607a6079a5674ea
jrTilak <tilakthapa9898@gmail.com>
2024-11-11 20:26:56 +0545

    feat: release of cli v2

--- src/cli/src/data/constant.ts
+++ src/cli/src/data/constant.ts
@@ -1,5 +1,4 @@
-// export const REGISTRY_URL = "https://lazykit.thapatilak.com.np";
-export const REGISTRY_URL = "http://localhost:3000";
+export const REGISTRY_URL = "https://lazykit.jrtilak.dev";
+// export const REGISTRY_URL = "http://localhost:3000";
 export const GITHUB_URL = "https://github.com/jrtilak/lazykit";
-export const SCHEMA_URL =
-  "https://lazykit.thapatilak.com.np/schema/config.json";
+export const SCHEMA_URL = "https://lazykit.jrtilak.dev/schema/config.json";
